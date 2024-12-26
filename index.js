6e18d3e8125daf41a4437cbc0cf1c84b716346e5
Harshit Raj <95363053+RajHarshit@users.noreply.github.com>
2024-12-26 23:05:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const h1 = document.getElementById("h-1")
+const p = document.getElementById("p")
+const btn = document.getElementById("btn")
+
+btn.addEventListener("click" ,function(){
+  fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(response => response.json()) // .then(function(response) {
+                                       //         return response.json()
+                                       //      })
+    .then(data => {
+      p.textContent = data.activity
+      h1.textContent = "🦾 HappyBot🦿"
+      document.querySelector("main").classList.add("fun")
+      btn.style.borderColor = "lightgreen "
+    })
+})
